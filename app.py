6989eed15231fa027d943e5c5e6514ba22ef2b00
clode.m.kim <clode.m.kim@polarisoffice.com>
2025-08-04 11:48:42 +0900

    app.py 문구 변경

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "✅ Hello from dynamic service!", 200
+    return "✅ 코드 변경 테스트", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
